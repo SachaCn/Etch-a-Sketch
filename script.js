85a5538f5efc0ece72079caaa1549c14d2ef7a48
Sacha <127155102+SachaCn@users.noreply.github.com>
2023-05-04 19:36:20 +0200

    Made some changes to the random color and darkening function

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const divContainer = document.querySelector('#container-main');
 const btn = document.querySelector('#btn');
 const root = document.querySelector(':root');
-let userChoice;
-let square;
 
-btn.addEventListener('click', () => {    
+btn.addEventListener('click', () => {
+    let userChoice;
     do {
-        userChoice = Number(prompt('Choose your grid size (max: 100)', 16));
-    } while (userChoice > 100)
+        userChoice = Number(prompt('Choose your grid size (max: 64)', 16));
+    } while (userChoice > 64)
     createGrid(userChoice);
-    square = document.querySelectorAll('.square');
-    square.forEach(ele => ele.addEventListener('mouseover', () =>{getRandomRgb(ele)}));
-    square.forEach(ele => ele.addEventListener('mouseover', () =>{changeBrightness(ele)})); 
+    let square = document.querySelectorAll('.square');
+    square.forEach(ele => ele.addEventListener('mouseover', () =>{randomHsl(ele)}));
+    square.forEach(ele => ele.addEventListener('mouseover', () =>{removeBrightness(ele)})); 
 });
 
 function createGrid(value) {
@@ -20,68 +19,23 @@ function createGrid(value) {
     for(let i = 0; i < value ** 2; i++) {
         const newDiv = document.createElement('div');       
         newDiv.classList.add('square');
-        newDiv.classList.add('bright100');
         divContainer.appendChild(newDiv);
     }   
 };
 
-function getRandomRgb(arg) {
-    let num = Math.round(0xffffff * Math.random());
-    let r = num >> 16;
-    let g = num >> 8 & 255;
-    let b = num & 255;
-    let rgb = `rgb(${r},${g},${b})`;
-    arg.style.backgroundColor = rgb;    
-  }
+function randomHsl(arg) {
+    arg.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+};
 
-// function genHexString(len) {
-//     let hex = '';
-//     for (let i = 0; i < len; ++i) {
-//         hex += (Math.floor(Math.random() * 16)).toString(16);
-//     }
-//     root.style.setProperty('--randomColor', `#${hex}`);
-// }
-
-function changeBrightness(arg) {
-    switch(true) {
-        case arg.classList.contains('bright100'):
-            arg.classList.replace('bright100', 'bright90');
-            break;
-        case arg.classList.contains('bright90'):
-            arg.classList.replace('bright90', 'bright80');
-            break;
-        case arg.classList.contains('bright80'):
-            arg.classList.replace('bright80', 'bright70');
-            break;
-        case arg.classList.contains('bright70'):
-            arg.classList.replace('bright70', 'bright60');
-            break;
-        case arg.classList.contains('bright60'):
-            arg.classList.replace('bright60', 'bright50');
-            break;
-        case arg.classList.contains('bright50'):
-            arg.classList.replace('bright50', 'bright40');
-            break;
-        case arg.classList.contains('bright40'):
-            arg.classList.replace('bright40', 'bright30');
-            break;    
-        case arg.classList.contains('bright30'):
-            arg.classList.replace('bright30', 'bright20');
-            break;
-        case arg.classList.contains('bright20'):
-            arg.classList.replace('bright20', 'bright10');
-            break;
-        case arg.classList.contains('bright10'):
-            arg.classList.replace('bright10', 'bright0');
-            break;
+function removeBrightness(arg) {
+    if (arg.style.filter.match(/brightness/)) {
+        let currentBrightness = Number(arg.style.filter.slice(-4, -2));
+        if (currentBrightness >= 10) {
+            arg.style.filter = `brightness(${currentBrightness - 10}%)`;
+        }
+    } else if (arg.style.filter == 'brightness(0%)') {
+        return;
+    } else {
+        arg.style.filter = 'brightness(90%)';
     }
-}
-
-
-
-// function decrementBrightness(arg) {     
-//     let value ; 
-//     if(value < 100) {
-//        arg.style.filter = `brightness(calc(100% - ${value += 10}%))`; 
-//     }        
-// }
+};
